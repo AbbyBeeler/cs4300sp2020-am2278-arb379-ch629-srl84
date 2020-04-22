f1b43fe1b9bb65fe14ac6c6b1703525587d8d098
Abby Beeler <abby.r.beeler@gmail.com>
2020-04-21 20:54:34 -0400

    more cleanup

--- server/app.py
+++ server/app.py
@@ -1,9 +1,7 @@
 from app import app, socketio
 import os
 
-port = int(os.environ.get("PORT", 5000))
-
 if __name__ == "__main__":
-  print("Flask app running at http://0.0.0.0:" + str(port))
-  socketio.run(app, host="0.0.0.0", port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    print("Flask app running at http://0.0.0.0:" + str(port))
+    socketio.run(app, host="0.0.0.0", port=port)
