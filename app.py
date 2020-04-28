431eeb9fd383e6867cf2b320ac2d34f8aae75f7f
Abby Beeler <abby.r.beeler@gmail.com>
2020-04-27 23:43:22 -0400

    heroku ignore fix and spacy fix

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from app import app, socketio
 import os
 import spacy
 
-spacy.cli.download('en_core_web_sm')
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print("Flask app running at http://0.0.0.0:" + str(port))
